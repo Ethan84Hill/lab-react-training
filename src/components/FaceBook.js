eb979c1e0d7de74048c1623908848fdbedbc2e1a
Ethan <ethan84hill@gmail.com>
2022-12-15 06:48:37 -0500

    iteration 13 done stuck on 14

--- src/components/FaceBook.js
+++ src/components/FaceBook.js
@@ -1,10 +1,24 @@
 
 import profiles from '../data/berlin.json';
 
-function FaceBook() {
+function FaceBook(props) {
+
+  const btnColor = {
+    backgroundColor: "red"
+}
+
     return(
         <div>
           <h1>FaceBook Profile List</h1>
+
+          <button onClick={() => { 
+            <p style={ btnColor }><b>Country: </b> {profiles.country}</p>
+            console.log('color change')
+          }}>USA</button>
+        
+
+
+
           {profiles.map(element => {
             return( 
             <div>
@@ -13,9 +27,9 @@ function FaceBook() {
                 </div>
 
                 <div>
-                   <p> <b>First Name:</b> {element.firstName}</p>
-                   <p><b>Last Name:</b> {element.lastName}</p>
-                   <p><b>Country:</b> {element.country}</p>
+                   <p><b>First Name: </b> {element.firstName}</p>
+                   <p><b>Last Name: </b> {element.lastName}</p>
+                   <p><b>Country: </b> {element.country}</p>
                 </div>
             </div>
             )
